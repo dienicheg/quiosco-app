dfab7ab857e3d896ecb1619fee19fb99d9f09c55
dienicheg <dienicheg.13@gmail.com>
2023-03-02 23:54:25 -0300

    primer commit

---
+++ pages/total.js
@@ -0,0 +1,55 @@
+import Layout from "layout/layout";
+import { useEffect, useCallback } from "react";
+import useQuiosco from "hooks/useQuiosco";
+import { formatearDinero } from "helpers";
+export default function Total() {
+  const { pedido, nombre, setNombre, colocarOrden, total} = useQuiosco();
+
+  const comprobarPedido = useCallback(() => {
+    return pedido.length === 0 || nombre === "" || nombre.length <= 3;
+  }, [pedido, nombre])
+
+  useEffect(() => {
+    comprobarPedido();
+  }, [pedido, comprobarPedido]);
+
+  
+
+  return (
+    <Layout pagina="Total">
+      <h1 className="text-4xl font-black">Total y Confirmar pedido</h1>
+      <p className="text-2xl my-10">Confirmar tu pedido a Continuación</p>
+
+      <form onSubmit={colocarOrden}>
+        <div>
+          <label
+            htmlFor="nombre"
+            className="block uppercase text-slate-800 font-bold text-xl"
+          >
+            Nombre
+          </label>
+          <input
+            type="text"
+            id="nombre"
+            className="bg-gray-200 w-full lg:w-1/3 p-2 mt-3 rounded-md"
+            value={nombre}
+            onChange={e => setNombre(e.target.value)}
+          />
+        </div>
+        <div className="mt-10 ">
+          <p className="text-2xl">
+            Total a pagar: <span className="font-bold">{formatearDinero(total)}</span>
+          </p>
+        </div>
+        <div className="mt-5">
+          <input
+            disabled={comprobarPedido()}
+            type="submit"
+            value="Confirmar Pedido"
+            className={`w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center ${comprobarPedido() ? "bg-indigo-300" : "bg-indigo-600"} hover:bg-indigo-800 cursor-pointer`}
+          />
+        </div>
+      </form>
+    </Layout>
+  );
+}
